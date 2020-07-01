2713f7a836a689af69a0d8b12bd69d5e84386814
Calvinoea <39657008+Calvinoea@users.noreply.github.com>
2020-07-01 17:39:35 +0100

    Create You-only-need-one.rb

---
+++ You-only-need-one.rb
@@ -0,0 +1,9 @@
+# You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+
+def check(arr,element)
+  #your code here
+  
+  arr.include? element
+  
+end
